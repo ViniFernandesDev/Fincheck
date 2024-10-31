cb612c7ce3baec96fbb3a9898e2b39cdc9762efc
Vinicius Fernandes <vinifernandesdeveloper@gmail.com>
2024-10-31 18:49:53 -0300

    feat/routes - private and public

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import { Router } from './Router'
 
 export function App() {
 
   return (
-    <>
-      <h1 className='bg-red-600 text-white'>teste</h1>
-    </>
+      <>
+        <Router />
+      </>
   )
 }
 
