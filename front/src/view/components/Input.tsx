7b54445806e7b5c6f0b9fde5c5b0e5778942d409
Vinicius Fernandes <vinifernandesdeveloper@gmail.com>
2024-11-04 09:48:01 -0300

    style/Input: add colors, transitions and label

--- front/src/view/components/Input.tsx
+++ front/src/view/components/Input.tsx
@@ -1,12 +1,25 @@
 import { ComponentProps } from "react";
 
-interface InputProps extends ComponentProps<'input'> {}
+interface InputProps extends ComponentProps<'input'> {
+    name: string;
+}
 
-export function Input(props: InputProps) {
+export function Input({ placeholder, name, id, ...props }: InputProps) {
+    const inputId = id ?? name;
+
     return (
-        <input 
-            {...props} 
-            className="bg-white rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800"
-        />
+        <div className="relative flex item-center">
+            <input 
+                id={inputId}
+                name={name}
+                {...props} 
+                className="bg-white w-full rounded-lg border border-gray-500 pt-4 px-3 h-[52px] text-gray-800 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none"
+                placeholder=" "
+            />
+
+            <label htmlFor={inputId} className="absolute text-xs left-[13px] top-2 pointer-events-none text-green-900 peer-placeholder-shown:text-gray-700 peer-placeholder-shown:text-base peer-placeholder-shown:top-3.5 transition-all">
+                {placeholder}
+            </label>
+        </div>
     )
 }
