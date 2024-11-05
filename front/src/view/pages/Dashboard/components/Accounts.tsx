27f54f93d4b2ff7c7d7d3cd466fe458309f87e1d
Vinicius Fernandes <vinifernandesdeveloper@gmail.com>
2024-11-05 14:09:21 -0300

    feat/create component dashboard and childrens

---
+++ front/src/view/pages/Dashboard/components/Accounts.tsx
@@ -0,0 +1,8 @@
+
+export function Accounts() {
+    return (
+        <div className="flex bg-teal-900 rounded-2xl w-full h-full px-4 py-6 md:p-10">
+            Accounts
+        </div>
+    )
+}
---
+++ front/src/view/pages/Dashboard/components/Transactions.tsx
@@ -0,0 +1,8 @@
+
+export function Transactions() {
+    return (
+        <div className="flex bg-gray-100 rounded-2xl w-full h-full px-4 py-6 md:p-10">
+            Transactions.tsx
+        </div>
+    )
+}
