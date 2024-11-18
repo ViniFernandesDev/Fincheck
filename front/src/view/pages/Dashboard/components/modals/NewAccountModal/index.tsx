e27738887aee6e77d1e58285234ec330efc5085d
Vinicius Fernandes <vinifernandesdeveloper@gmail.com>
2024-11-18 10:36:16 -0300

    feat/component Modal New Account with input recurrency and Select

--- front/src/view/pages/Dashboard/components/modals/NewAccountModal/index.tsx
+++ front/src/view/pages/Dashboard/components/modals/NewAccountModal/index.tsx
@@ -1,4 +1,8 @@
+
+import { Input } from "../../../../../components/Input";
+import { InputCurrency } from "../../../../../components/InputCurrency";
 import { Modal } from "../../../../../components/Modal";
+import { Select } from "../../../../../components/Select";
 
 import { useNewAccountModalController } from "./useNewAccountModalController";
 
@@ -10,7 +14,46 @@ export function NewAccountModal() {
 
     return (
         <Modal open={isNewAccountModalOpen} onClose={closeNewAccountModal} title="Nova conta">
-            Criar conta
+            <form action="">
+                <div>
+                    <span className="text-gray-600 tracking-[-0.5px] text-xs">
+                        Saldo
+                    </span>
+
+                    <div className="flex justify-center items-center gap-2">
+                        <span className="text-gray-600 tracking-[-0.5px] text-lg">
+                            R$
+                        </span>
+                        <InputCurrency/>
+                    </div>
+                </div>
+
+                <div className="mt-10 flex flex-col gap-4">
+                    <Input
+                        type="text"
+                        name="nameAccount"
+                        placeholder="Nome da conta"
+                    />
+
+                    <Select 
+                        placeholder="Tipo" 
+                        options={[
+                            {
+                                value: 'INVESTMENT',
+                                label: 'Investimentos'
+                            },
+                            {
+                                value: 'CASH',
+                                label: 'Dinheiro Físico'
+                            },
+                            {
+                                value: 'CHECKING',
+                                label: 'Conta corrente'
+                            },
+                        ]} 
+                    />
+                </div>
+            </form>
         </Modal>
     )
 }
